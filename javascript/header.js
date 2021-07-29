76e13a3d81d78a8c2272eb33c5be6a8914c5946b
Lamnq99 <lam.nq@haposoft.com>
2021-07-29 10:36:04 +0700

    test

--- javascript/header.js
+++ javascript/header.js
@@ -1,24 +1,14 @@
 $('.navbar-light .navbar-nav .nav-link').on('click', function () {
   $('.navbar-light .navbar-nav .nav-link').removeClass('active');
   $(this).addClass('active');
-  // if ($('.custom-navbar-collapse').hasClass('show')) {
-  //     $('.custom-navbar-collapse').removeClass('show')
-  //     $('.custom-button-navbar-toggler').removeClass('collapsed');
-  //     $('.custom-button-navbar-toggler .fas.fa-bars').removeClass('hidden');
-  //     $('.custom-button-navbar-toggler .fas.fa-times').addClass('hidden');
-  //     $('.addition-class').addClass('hidden');
-  //     $('#body').removeClass('hidden');
-  //     $('footer').removeClass('hidden');
-  // }
-
-
 });
 
 $(document).ready(function () {
   $('.btn-header').click(function () {
     if ($(".collapse").hasClass("show")) {
       $(".collapse").removeClass("show");
-      $(".btn-navbar .navbar-toggler .navbar-toggler-icon").css({ "display": "none" })
+      $(".navbar-toggler-icon").css({ "display": "inline-block" })
+      $(".img-close-header").css({ "display": "none" })
     }
   });
 
